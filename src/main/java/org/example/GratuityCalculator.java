dc16015a0d75945583942f790d02dd8ea44ca6e6
shahJainam961 <shahjainam961@gmail.com>
2023-11-20 23:02:03 +0530

    Gratuity Calculator done

---
+++ src/main/java/org/example/GratuityCalculator.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class GratuityCalculator {
+    private Double monthlySalary;
+    private Double yearsOfService;
+
+    public Double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(Double monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    public Double getYearsOfService() {
+        return yearsOfService;
+    }
+
+    public void setYearsOfService(Double yearsOfService) {
+        this.yearsOfService = yearsOfService;
+    }
+
+    public Long init(){
+        Double ms, yos;
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.print("Enter your monthly salary amount : ");
+            ms = scanner.nextDouble();
+            if(ms>=0){
+                break;
+            }
+            System.out.println("Please enter positive monthly salary : ");
+        }
+        setMonthlySalary(ms);
+
+        while(true){
+            System.out.print("Enter years of service : ");
+            yos = scanner.nextDouble();
+            if(yos>=0){
+                break;
+            }
+            System.out.println("Please enter valid year of service");
+        }
+        setYearsOfService(yos);
+
+        Long totalValue = calculateReturn();
+        System.out.println("You are eligible for " + totalValue + " gratuity");
+        return totalValue;
+    }
+
+    public Long calculateReturn(){
+        Double amnt = getYearsOfService() * getMonthlySalary() * 15 / 26;
+        return Math.min(1000000, amnt.longValue());
+    }
+
+}
