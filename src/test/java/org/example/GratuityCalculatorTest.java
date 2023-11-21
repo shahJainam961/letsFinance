a820f09221778c14c884371a8ec56914257f1603
shahJainam961 <shahjainam961@gmail.com>
2023-11-21 17:58:13 +0530

    Gratuity Calculator - Data flow coverage criteria testing done

--- src/test/java/org/example/GratuityCalculatorTest.java
+++ src/test/java/org/example/GratuityCalculatorTest.java
@@ -5,27 +5,27 @@ import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 
-public class EMICalculatorTest {
+import static org.junit.Assert.*;
 
-    String input1 = "1000000\n5.5\n2\n"; // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    String input2 = "-10000\n2000000\n-5\n3.5\n-2\n2\n"; // [0, 1, 2, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 7, 8, 9, 10, 11]
+public class GratuityCalculatorTest {
+    String input1 = "3400\n12\n"; // [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    String input2 = "-10000\n2300\n-5\n12\n"; // [0, 1, 2, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8]
 
     public void testing(String input, Long expectedTax){
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(input.getBytes());
         System.setIn(byteArrayInputStream);
-        EMICalculator emiCalculator = new EMICalculator();
-        Long actual = emiCalculator.init();
+        GratuityCalculator gratuityCalculator = new GratuityCalculator();
+        Long actual = gratuityCalculator.init();
         Assert.assertEquals(expectedTax,actual);
     }
 
     @Test
     public void testCase1(){
-        testing(input1, 44095L);
+        testing(input1, 23538L);
     }
 
     @Test
     public void testCase2(){
-        testing(input2, 86405L);
+        testing(input2, 15923L);
     }
-
 }
